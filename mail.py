a18d4ac32699a1bf50fdf885fe6892e22b107f39
Weirdo <weirdo@localhost.localdomain>
2021-05-24 03:55:20 +0100

    infinity sender

--- mail.py
+++ mail.py
@@ -1,6 +1,9 @@
 #sender
 import smtplib, ssl
 from datetime import datetime
+from threading import Timer
+from time import *
+
 port = 465
 smtp = "mail-serwer64733.lh.pl" 
 
@@ -21,6 +24,17 @@ message="""Subject: {}
 """.format(subject,mess)
 context = ssl.create_default_context()
 
-with smtplib.SMTP_SSL(smtp, port, context=context) as server:
-	 	server.login("pytania@greenstar.net.pl",password)
-	 	server.sendmail(sender,reciver,message)
+
+def send():
+	with smtplib.SMTP_SSL(smtp, port, context=context) as server:
+	 server.login("pytania@greenstar.net.pl",password)
+	 server.sendmail(sender,reciver,message)
+	 print("wysłane")
+
+b=0
+while b<=5:
+	a=10.0
+	t=Timer(a,send)
+	t.start()
+	sleep(a)
+	t.cancel()
